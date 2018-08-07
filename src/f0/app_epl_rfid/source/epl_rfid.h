94d8fc086571ed11eff5955bbdf968ef1067dacb
Chad Coates <chroco@gmail.com>
2018-08-07 07:56:13 -0700

    switching to the esp32:(

--- src/f0/app_epl_rfid/source/epl_rfid.h
+++ src/f0/app_epl_rfid/source/epl_rfid.h
@@ -10,18 +10,14 @@
 
 #define PWM_TIMER_FREQ	1e6 /// Hz
 #define PWM_FREQ				125e3 /// periods per sec
-//#define PWM_TIMER_FREQ	48e6 /// Hz
-//#define PWM_FREQ				5e3 /// periods per sec
-//#define PWM_TIMER_FREQ	10e3 /// Hz
-//#define PWM_FREQ				1 /// periods per sec
 #define PWM_PERIOD			PWM_TIMER_FREQ/PWM_FREQ /// 100 ticks/period @ 8us/tick
 #define PWM_CH1					0
 
-#define ICU_FREQ	12500 // Hz
-//#define ICU_FREQ	PWM_TIMER_FREQ //10e3 // Hz
+#define ICU_FREQ	PWM_TIMER_FREQ //10e3 // Hz
 
 #define DEBUG_SERIAL SD2
 #define DEBUG_CHP ((BaseSequentialStream *) &DEBUG_SERIAL)
+
 extern THD_WORKING_AREA(waThread_epl_rfid,THREAD_SIZE);
 extern THD_FUNCTION(Thread_epl_rfid, arg);
 
